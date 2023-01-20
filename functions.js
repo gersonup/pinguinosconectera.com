c613ab1add8123540e9399484bb28be22ca9cebf
Gerson Ricardo Navarro Hernandez <gersonup@gmail.com>
2023-01-20 17:32:31 -0500

    Create function to create barriers randomly

--- functions.js
+++ functions.js
@@ -2,10 +2,19 @@ $(document).ready(function(){
     $("#btn-draw").click(function(e) {
         e.preventDefault();
 
+        // Elements Dictionary
+        const elementos = {
+            "wall"      : '<div class="wall"><b>W</b></div>',
+            "barrier"   : '<div class="barrier"><b></b>B</div>',
+            "penguin1"  : '<div class="penguin1"><b></b>P1</div>',
+            "penguin2"  : '<div class="penguin2"><b></b>P2</div>',
+            "goal"      : '<div class="goal"><b></b>G</div>'
+        }
+
         //Number of barriers
         let n = $("#n").val();
-        let VidaX = $("#vidaX").val();
-        let VidaY = $("#vidaY").val();
+        let vidaX = $("#vidaX").val();
+        let vidaY = $("#vidaY").val();
         let pinguino1X = $("#pinguino1X").val();
         let pinguino1Y = $("#pinguino1Y").val();
         let pinguino2x = $("#pinguino2X").val();
@@ -14,17 +23,66 @@ $(document).ready(function(){
         if (n >= 10) {
             createLaberinto(n);
         } else {
-            alert("n debe ser almenos de un valor de 10.");
+            alert("La medida minima es 17.");
+        }
+
+        function createBarries(matrixLength) {
+            var maxNumBarriers = Math.floor(((Math.pow(matrixLength, 2) * 30)/100) + 1);
+
+            console.log("power2: " + Math.pow(matrixLength, 2));
+            console.log("maxnumBarriers:" + maxNumBarriers);
+
+            var barriers = new Array();
+            var barrier = new Array();
+            for (let i = 0; i < maxNumBarriers; i++) {
+                barriers[i] = new Array();
+                barrier[i] = new Array();
+                for (let j = 0; j < 2; j++) {
+                    barrier[i][j] = Math.floor((Math.random() * matrixLength) + 1);
+                }
+                if (!barriers.includes(barrier[i])) {
+                    barriers[i] = barrier[i]
+                }
+            }
+
+            console.log(barriers);
+            return barriers;
         }
 
         function createLaberinto(size) {
-            $("#laberinto").append(`<div class="mapa" id></div>`);
-            for (let j = 0; j < size; j++) {
-                for (let k = 0; k < size; k++) {
-                    $(".mapa").append(`<div class="position" id="${k}></div>`);
+            console.clear();
+
+            var barriers = createBarries(size);
+            var row = new Array();
+
+            // Create matrix
+            for (let i = 0; i < size; i++) {
+                row[i] = new Array();
+                for (let j = 0; j < size; j++) {
+                    row[i][j] = 0;
                 } 
             }
-            $(".mapa").css({ "width": `${size * 25}`, "height": `${size * 25}`, "grid-template-columns": `repeat(${size}, 1fr)`, "grid-template-rows": `repeat(${size}, 1fr)` });  
+            
+            console.log(row);
+            console.log(row.length);
+            //console.log("position: " + row[vidaX][vidaY]);
+            
+            //Random function
+            //Math.floor((Math.random() * 1) + 1);
+
+            /* $("#laberinto").empty();
+            $("#laberinto").append(`<div class="mapa"></div>`);
+            for (let j = 0; j < size; j++) {
+                for (let k = 0; k < size; k++) {
+                    $(".mapa").append(`<div class="position" id="div-${j}-${k}></div>`);
+                } 
+            }
+            $(".mapa").css({ 
+                "width": `${size * 30}`, 
+                "height": `${size * 30}`, 
+                "grid-template-columns": `repeat(${size}, 1fr)`, 
+                "grid-template-rows": `repeat(${size}, 1fr)` 
+            });   */
         }
     });
 });
